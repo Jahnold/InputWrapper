9ab4c84e48f061906d00594d23fb9b84b371beb7
Matt Arnold <matt@arnold.codes>
2020-04-23 17:12:34 +0100

    custom behaviour

--- app/src/main/java/codes/arnold/inputwrapper/MainActivity.kt
+++ app/src/main/java/codes/arnold/inputwrapper/MainActivity.kt
@@ -3,6 +3,7 @@ package codes.arnold.inputwrapper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import codes.arnold.inputwrapper.view.InputWrapper
+import codes.arnold.inputwrapper.view.behaviours.TextCaseBehaviour
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
 
         val errorLength = findViewById<InputWrapper>(R.id.error_length)
         errorLength.setValidator { text -> text.length > 5 || text.isEmpty() }
+
+        val customBehaviour = findViewById<InputWrapper>(R.id.custom_behaviour)
+        customBehaviour.setBehaviour(TextCaseBehaviour(customBehaviour))
     }
 }
